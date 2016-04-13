8e38025b83c6662c57abd57cc7aa0c97d0c77be6
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-04-13 13:27:16 -0500

    Alphabetized JS and sectioned it off so that variables, functions, and function calls have discreet locations in code. Reduced repeated code by creating two new functions so that they can be called downstream.

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -4,38 +4,56 @@ $(document).ready(function() {
 // Looping audio that starts once start button is pressed
 var audio = new Audio('/sound/pkmnbattle.mp3');
 audio.loop = true;
-// player score variable
-var score = 0;
-// timer variable
-var timer = 30;
 // high score variable
 var highScore = 0;
+// player score variable
+var score = 0;
 //rate moles show up
 var speed = 2000;
-// random number generation
-var x = Math.floor((Math.random() * 9) + 1);
-// displays score
-$('#score').html('Score: ' + score);
-// displays time remaining
-$('#timer').html('Time Remaining: ' + timer+'s');
-// displays high score
-$('#highScore').html('High Score: ' + highScore);
-
+// timer variable
+var timer = 30;
 // Generates random number to reference for mole positioning
 var whackPattern = setInterval(function() {
     x = Math.floor((Math.random() * 9) + 1);
     }, 1000);
-
-// Constantly and rapidly checks high score to make it appear that high score changes as soon as a new high score is reached
-var checkHighScore = setInterval(function() {
-        if (score > highScore){
-            highScore = score;
-            $('#highScore').html('High Score: ' + highScore);
-        }
-    }, 20);
+// random number generation
+var x = Math.floor((Math.random() * 9) + 1);
 
 // initially hides moles so that they can fadeIn and out with the random number generator.
 $('.diglett').fadeOut('slow');
+// displays high score
+$('#highScore').html('High Score: ' + highScore);
+// displays score
+$('#score').html('Score: ' + score);
+// displays time remaining
+$('#timer').html('Time Remaining: ' + timer+'s');
+
+//resets score and logs refreshed score
+function callScore(){
+    score = 0;
+    $('#score').html('Score: ' + score);
+};
+
+//resets timer and logs refreshed timer
+function callTimer(){
+    timer = 30;
+    $('#timer').html('Time Remaining: ' + timer+'s');
+};
+
+// Ends game when timer equals zero. Clears intervals that cause moles to appear and timer to increment. 
+function endGame(){
+    if (timer == 0){
+        clearInterval(gameInterval);
+        clearInterval(timeInterval);
+        audio.pause();
+    };
+};
+
+//Add points to score
+function increaseScore(){
+    score++;
+    $('#score').html('Score: ' + score);
+};
 
 // Randomly causes a mole to appear in one of nine div boxes.
 function startGame(){
@@ -64,12 +82,6 @@ function startGame(){
     }, speed);
 };
 
-//Add points to score
-function increaseScore(){
-    score++;
-    $('#score').html('Score: ' + score);
-};
-
 // Timer increments every second and logs time remaining.
 function startTimer(){
     score = 0;
@@ -81,45 +93,37 @@ function startTimer(){
     }, 1000);
 };
 
-// Ends game when timer equals zero. Clears intervals that cause moles to appear and timer to increment. 
-function endGame(){
-    if (timer == 0){
-        clearInterval(gameInterval);
-        clearInterval(timeInterval);
-        audio.pause();
-    };
-};
-
-// reset button begins disabled
-$('#resetButton').attr('disabled', true);
-
-// start button starts game, disables start button from being clicked again, and allows player to reset the
-// game by clicking the reset button.
-$('#startButton').click(function(){
-    score = 0;
-    $('#score').html('Score: ' + score);
-    timer = 30;
-    $('#timer').html('Time Remaining: ' + timer+'s');
-    startGame();
-    startTimer();
-    $(this).attr('disabled', true);
-    $('#resetButton').attr('disabled', false);
-});
-
-// Clears player score and sets timer back to 30s for 'new game'
-$('#resetButton').click(function(){
-    score = 0;
-    $('#score').html('Score: ' + score);
-    timer = 30;
-    $('#timer').html('Time Remaining: ' + timer+'s');
-    speed=1500;
-});
-
-// Increases player score, mole appearance rate, and causes mole to disappear when clicked
+// Increases player score, mole appearance rate, and causes mole to disappear when clicked. 
+// Also increases high score when score is greater than current high score.
 $('#box1, #box2, #box3, #box4, #box5, #box6, #box7, #box8, #box9').click(function(){
     increaseScore();
     $('.diglett').hide();
     speed-=100;
+    if (score > highScore){
+    highScore = score;
+    $('#highScore').html('High Score: ' + highScore);
+    };
+});
+
+// reset button begins disabled
+$('#resetButton').attr('disabled', true);
+
+// Clears player score and sets timer back to 30s for 'new game'
+$('#resetButton').click(function(){
+    callScore();
+    callTimer();
+    speed=1500;
+});
+
+// start button starts game, disables start button from being clicked again, and allows player to reset the
+// game by clicking the reset button.
+$('#startButton').click(function(){
+    callScore();
+    startGame();
+    callTimer();
+    startTimer();
+    $(this).attr('disabled', true);
+    $('#resetButton').attr('disabled', false);
 });
 
 });
